872fba7c117215132eb11dca25a7f79ba7277412
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2025-07-24 08:48:40 +0300

    add new pruning metric

--- act_prune/modelling/layers/linear_act_sp.py
+++ act_prune/modelling/layers/linear_act_sp.py
@@ -40,6 +40,29 @@ class Linear_act_sp(nn.Module):
         x_sp = x * mask
         return x_sp
 
+    def semi_structural_L_pruner(self, x, prune_n=2, prune_m=4):
+        """
+        If we remove X_{it} from the input activation X:
+            L_{cos_{ti}} = |X_it| * sqrt(sum_p X_pt^2)  / sqrt(sum_j X_ij^2)
+        h is hidden dimension, l is sequence length.
+        """
+    
+        abs_x = torch.abs(x_flat) # |X_it|
+        denominator = torch.sqrt(torch.sum(x ** 2, dim=1, keepdim=True))  # sqrt(sum_j X_ij^2)
+        col_norms = torch.sqrt(torch.sum(x ** 2, dim=0, keepdim=True))  # sqrt(sum_p X_pt^2)
+    
+        L_metric = abs_x / (denominator + 1e-8) * col_norms
+    
+        orig_shape = L_metric.shape
+        L_metric_1d = L_metric.view(-1, prune_m)
+    
+        _, idx = torch.topk(L_metric_1d, prune_n, dim=1, largest=False, sorted=False)
+        mask_1d = torch.ones_like(L_metric_1d, dtype=torch.bool)
+        mask_1d.scatter_(dim=1, index=idx, value=False)
+        mask = mask_1d.view(orig_shape).view_as(x)
+        x_sp = x * mask
+        return x_sp
+
     def forward (self, x):
         bs, seq_len, _ = x.shape
         x_flat = x.view(-1, self.in_features)
@@ -53,6 +76,13 @@ class Linear_act_sp(nn.Module):
                 x_flat, 
                 prune_n=self.prune_n, prune_m=self.prune_m)
             out = x_flat_sp @ self.weight.t()
+
+        elif self.sparsity_type == "semi_structural_L_pruner":
+            x_flat_sp = self.semi_structural_L_pruner(
+                x_flat,
+                prune_n=self.prune_n, 
+                prune_m=self.prune_m
+            )
 
         elif self.sparsity_type == "semi-structured_shift":
             eta = torch.mean(x_flat, dim=1, keepdim=True)  # eq (9)
